a283a465fbe2c68a0deab58274b1a835dbadb253
Toby Law <toby@tzcl.me>
2022-09-18 17:50:37 +1000

    feat: installed svelte/tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import image from "@astrojs/image";
+import svelte from "@astrojs/svelte";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image()]
+  integrations: [react(), image(), svelte(), tailwind()]
 });
