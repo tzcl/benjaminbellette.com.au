628ab8728b9de315b2eae016887c062cafd67a9e
Toby Law <toby@tzcl.me>
2022-10-14 21:57:25 +1100

    deps: remove @astrojs/image for now
    Can add it back in when I need it, but it's extra complexity that I
    don't need right now.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import image from "@astrojs/image";
 import svelte from "@astrojs/svelte";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image(), svelte(), tailwind()]
+  integrations: [react(), svelte(), tailwind()],
 });
