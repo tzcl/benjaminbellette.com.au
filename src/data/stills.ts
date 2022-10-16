f9016646d2e538a43ef11f779be2d1acbf414b61
Toby Law <toby@tzcl.me>
2022-10-16 20:17:01 +1100

    feat(stills): refactored data

---
+++ src/data/stills.ts
@@ -0,0 +1,225 @@
+import type { Item } from "../components/dynamic/Tile.svelte";
+
+export const hero = [
+  "https://i.imgur.com/VmNhVhL.jpg",
+  "https://i.imgur.com/0bVicqt.jpg",
+  "https://i.imgur.com/vFJOq8s.jpg",
+];
+
+export const bts: Item[] = [
+  {
+    type: "img",
+    src: "https://i.imgur.com/22bOxRj.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/USpuWXu.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/gEPHCCL.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/AcON5os.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/ezeE3g1.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/RAJYFMe.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/wt8sYFZ.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/ySvlLT8.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/wpLPzuc.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/PlC9rqh.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/Zxa4KRM.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/hwXVxsA.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/PCKxfBR.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/YIjQI0n.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/5LBXHbR.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/wQzJB9e.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/ZbjWRGL.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/aJJiuCa.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/23AuO4P.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/pVmy300.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/Xl5zux4.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/4znchdF.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/hbaEKwe.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/MD5Mtep.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/vIwQfs6.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/53epSLY.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/byXw3Al.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/7rrfmZ7.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/yCzB65P.jpg",
+    title: "Stills",
+  },
+];
+
+export const photography: Item[] = [
+  {
+    type: "img",
+    src: "https://i.imgur.com/bYNd8Q7.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/MfGJYnM.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/HWmXZmc.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/LIZM1Cm.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/ROsM6Cl.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/DfelLvX.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/YsCjVjz.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/znO6Zyk.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/EIA2emQ.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/7vyuJpC.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/VoRxD4E.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/ckFMfwr.jpg",
+    title: "Stills",
+  },
+  {
+    type: "img",
+    src: "https://i.imgur.com/CvhCbiA.jpg",
+    title: "Stills",
+  },
+];
+
+export default { hero, bts, photography };
