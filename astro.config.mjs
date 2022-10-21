481556b2060994ca1787717cda2f2e8c8cd8acb6
Toby Law <toby@tzcl.me>
2022-10-21 21:02:55 +1100

    feat(astro): install integrations
    Installed sitemap, critters, compress and image.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,26 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import critters from "astro-critters";
+import compress from "astro-compress";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
     svelte(),
+    image(),
     tailwind({
       // Disable injecting a basic `base.css` on every page
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
+    sitemap(),
+    compress(),
+    critters(),
   ],
 });
